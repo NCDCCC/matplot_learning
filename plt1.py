3dede510acfa3648249dfcdc9aeafd88644d6af6
NCDCCC <ZhangZQ0601@outlook.com>
2018-10-25 10:42:44 +0800

    improve

--- plt1.py
+++ plt1.py
@@ -15,6 +15,7 @@ new_ticks = np.linspace(-1,2,5)
 plt.xticks(new_ticks)
 plt.yticks([-2,-1.8,-1,1.22,3],
 	[r'$blue$',r'$pink$',r'$medium$',r'$medium well$',r'$well done$'])
+
 #gca = 'get current axis'
 ax = plt.gca()
 #hide top & right spines
@@ -27,8 +28,31 @@ ax.yaxis.set_ticks_position('left')
 ax.spines['bottom'].set_position(('data',0))
 ax.spines['left'].set_position(('data',0))
 ########
-l1, =plt.plot(x,y1,label='y=x')
-l2, =plt.plot(x,y2,color='red',linestyle='--',label='y=x^2')
+l1, =plt.plot(x,y1,label=r'$y=x*2$')
+l2, =plt.plot(x,y2,color='red',linestyle='--',label=r'$y=x^2$')
 plt.legend(handles=[l1,l2],loc='best')
 ########
+
+x0 = 1
+y0 = x0**2
+#size of point
+plt.scatter(x0,y0,s = 66,color = 'b')
+plt.plot([x0,x0],[y0,0],'k-.',lw = 2.5)
+
+#tips of points
+plt.annotate(r'$x^2=%d$' % y0,
+	xy=(x0,y0),
+	xycoords='data',
+
+	xytext=(+30,-30),
+	textcoords='offset points',
+	fontsize=16,
+	arrowprops=dict(arrowstyle='->',connectionstyle='arc3,rad=.2')
+	)
+
+plt.text(0,3,
+	r'$Kinds\ of\ steak.\alpha\ \mu\ \sigma$',
+	fontdict={'size':16,'color':'r'})
+
+
 plt.show()
