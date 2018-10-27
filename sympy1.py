bc1e42ede0fed892c1f652510741ac2fe0b4b68c
NCDCCC <ZhangZQ0601@outlook.com>
2018-10-27 10:54:26 +0800

    add diff() series()

--- sympy1.py
+++ sympy1.py
@@ -1,5 +1,5 @@
 from sympy import *
-
+from sympy.abc import a,b
 x = Symbol('x')
 y = Symbol('y')
 z = Symbol('z')
@@ -13,8 +13,6 @@ pprint(e.simplify())
 f = sin(x)/cos(x)
 pprint(y.trigsimp())
 
-a = Symbol('a')
-b = Symbol('b')
 g = x**a*x**b
 pprint(g.powsimp())
 
@@ -22,3 +20,15 @@ pprint(solve([y+x/4,x-13-4*z,4*x-y+z-1],[x,y,z]))
 
 h = 2*x**2*(x-(sin(x))**2*cos(x**2)*E**sin(x))
 pprint(series(h))
+
+i = x/(x-1)
+pprint(series(i),x,2)
+
+q = x**2*log(x)
+pprint(series(q,x,1))
+
+for i in range(1,6):
+	pprint(diff(q,x,i))
+
+p = x - (a+b*cos(x))*sin(x)
+pprint(series(p,x,0))
